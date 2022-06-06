ea6b1feb6114f8ce0d20dee8eeba9103fc0a8d92
Kevin <kevin.511@outlook.com>
2022-06-06 16:40:11 -0500

    Creando cuarto comp custom

---
+++ react/components/PdfReader.tsx
@@ -0,0 +1,35 @@
+import React, {useEffect,useState} from "react";
+
+type Props = {
+    pdfUrl: string,
+    width: number,
+    height: number
+}
+
+const PdfReader = ({pdfUrl, width, height}: Props) => {
+    const [mounted, setMounted] = useState(false)
+
+    useEffect(() => {
+        setMounted(true)
+    }, [])
+
+return (
+    mounted && (
+        <div className="flex justify-center">
+            <object
+                data = {pdfUrl}
+                type = 'application/pdf'
+                width = {width}
+                height = {height}
+            >
+                <iframe title = "PDF" src = {pdfUrl} width={width} height ={height}>
+                    <p>This browser does not support PDF files</p>
+                </iframe>
+            </object>
+        </div>
+    )
+)
+
+}
+
+export default PdfReader
